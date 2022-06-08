ae67ba2bd147cf07f4936cd9a2988a80c1c3a6da
Daniel Kostro <kostro.d@gmail.com>
2022-06-08 14:21:02 +0200

    chore: segue into rest-on-couch process from bash process

--- rest-on-couch/start.sh
+++ rest-on-couch/start.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 /create_db.sh
 
-node /rest-on-couch-source/bin/rest-on-couch-server.js
+https://unix.stackexchange.com/a/196053/116499
+exec node /rest-on-couch-source/bin/rest-on-couch-server.js
