be0f1624ea2bfa2810343dcc5e68527c97ad16e3
Daniel Kostro <kostro.d@gmail.com>
2026-01-28 11:55:39 +0100

    refactor: change how promise rejection is tested on `getQuery`

--- src/roc/__tests__/helper.test.ts
+++ src/roc/__tests__/helper.test.ts
@@ -1,21 +1,25 @@
-import { expect, test } from 'vitest';
+import { AxiosError } from 'axios';
+import { describe, expect, it } from 'vitest';
 
 import { getRocClientError } from '../../index.ts';
 import { testRoc } from '../../testUtils.ts';
 
-test('getRocClientError', async () => {
-  try {
-    await testRoc.getQuery('entryById');
+describe('Axios errors', () => {
+  it('is an axios error', async () => {
+    await expect(testRoc.getQuery('entryById').fetch()).rejects.toBeInstanceOf(
+      AxiosError,
+    );
+  });
 
-    // Make sure tests fails if the statement above does not throw
-    expect(false).toBe(true);
-  } catch (e) {
-    const rocError = getRocClientError(e);
-
-    // eslint-disable-next-line vitest/no-conditional-expect
-    expect(rocError).toMatchObject({
-      code: 'unauthorized',
-      error: 'entryById is not a view with owner',
-    });
-  }
+  it('error data has been fetched', async () => {
+    await expect(testRoc.getQuery('entryById').fetch()).rejects.toSatisfy(
+      (error) => {
+        const errorData = getRocClientError(error);
+        return (
+          errorData?.code === 'unauthorized' &&
+          errorData?.error === 'entryById is not a view with owner'
+        );
+      },
+    );
+  });
 });
